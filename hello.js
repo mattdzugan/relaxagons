5b32e84c2a9e5b1b48ecc8f00c9a4606c219fe8b
Matt Dzugan <mattdzugan@gmail.com>
2021-02-22 22:54:37 -0600

    tweets working

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 d3 = require("d3");
 turf = require("turf");
 var spawn = require('child_process').spawn;
+const Twitter = require("twitter");
 const fs = require('fs');
 const { createCanvas } = require('canvas');
 pc = require("./polygonClip.js");
@@ -547,13 +548,109 @@ function postTweet(){
   console.log(" ")
   console.log("🐦 👋 🐦 👋 🐦 👋 🐦 👋 🐦")
   console.log(" ")
-}
+
+
+  const client = new Twitter({
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token_key:     '',
+    access_token_secret:  ''
+  })
+
+
+  const pathToFile = '/Users/mdzugan/Documents/GitHub/relaxagons/out.mp4';
+  const mediaType = "video/mp4"
+
+  const mediaData = fs.readFileSync(pathToFile)
+  const mediaSize = fs.statSync(pathToFile).size
+
+  initializeMediaUpload()
+    .then(appendFileChunk)
+    .then(finalizeUpload)
+    .then(publishStatusUpdate)
+
+  function initializeMediaUpload() {
+    return new Promise(function(resolve, reject) {
+      client.post("media/upload", {
+        command: "INIT",
+        total_bytes: mediaSize,
+        media_type: mediaType
+      }, function(error, data, response) {
+        if (error) {
+          console.log(error)
+          reject(error)
+        } else {
+          resolve(data.media_id_string)
+        }
+      })
+    })
+  }
+
+  function appendFileChunk(mediaId) {
+    return new Promise(function(resolve, reject) {
+      client.post("media/upload", {
+        command: "APPEND",
+        media_id: mediaId,
+        media: mediaData,
+        segment_index: 0
+      }, function(error, data, response) {
+        if (error) {
+          console.log(error)
+          reject(error)
+        } else {
+          resolve(mediaId)
+        }
+      })
+    })
+  }
+
+  function finalizeUpload(mediaId) {
+    return new Promise(function(resolve, reject) {
+      client.post("media/upload", {
+        command: "FINALIZE",
+        media_id: mediaId
+      }, function(error, data, response) {
+        if (error) {
+          console.log(error)
+          reject(error)
+        } else {
+          resolve(mediaId)
+        }
+      })
+    })
+  }
+
+  function publishStatusUpdate(mediaId) {
+    return new Promise(function(resolve, reject) {
+      client.post("statuses/update", {
+        status: "⬢⬡⬢⬡⬢⬡ #satisfying",
+        media_ids: mediaId
+      }, function(error, data, response) {
+        if (error) {
+          console.log(error)
+          reject(error)
+        } else {
+          console.log("Successfully uploaded media and tweeted!")
+          resolve(data)
+        }
+      })
+    })
+  }
 
 
 
 
 
-//*
+
+
+
+} // end of post tweet
+
+
+
+
+
+/*
 foundWinner = false;
 while (!foundWinner){
   params = generateParams();
@@ -575,4 +672,5 @@ while (!foundWinner){
 }
 
 //*/
-//generateVideo();
+generateVideo();
+//postTweet();
